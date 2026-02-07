6414040f120e398435c19711f636deb3457d7a69
Ibrahim AbdElwahab <iabdwahab20@gmail.com>
2026-02-07 02:22:43 +0200

    Add announcements bar with Swiper carousel
    Introduce a new AnnouncementsBar component that uses Swiper to render a continuous, autoplaying announcements ticker (Arabic messages). Integrate the component into SubHeader and replace the previous static announcements area. Add swiper as a dependency in package.json and include minimal CSS tweaks for .swiper-wrapper and .swiper-slide to ensure smooth linear scrolling and proper slide height.

--- src/components/header/SubHeader.jsx
+++ src/components/header/SubHeader.jsx
@@ -1,6 +1,7 @@
 // import { Link } from 'react-router-dom';
 
 import { Link } from 'react-router-dom';
+import AnnouncementsBar from '../AnnouncementsBar';
 
 const SubHeader = () => {
   return (
@@ -10,14 +11,16 @@ const SubHeader = () => {
           إعلامات وتحديثات المنصة: <a href="https://t.me/darelom_students">قناة التليجرام.</a>
         </div>
       </div>
-      {/* <div className='subheader bg-dark border-top text-light fs-6' style={{ padding: '11px 0' }}>
-        <div className='container-xl text-center'>
-          يمكنكم الآن الاطلاع على: {''}
-          <br />
-          <span>ترتيب نتائج الفصل الدراسي الثاني 2025.</span>
+      <AnnouncementsBar />
 
-        </div>
-      </div> */}
+      <div
+        className="subheader text-center py-3 bg-dark border-top text-light fs-6"
+        style={{ padding: '11px 0' }}
+      >
+        <a href="https://darelom-students.netlify.app">
+          رابط ترتيب نتائج الفصل الدراسي الأول - 2026.
+        </a>
+      </div>
       {/* <div className='subheader bg-dark border-top text-light fs-6' style={{ padding: '11px 0' }}>
         <div className='container-xl text-center'>
           يمكنكم الآن الاطلاع على ترتيب الفرق 2025.
